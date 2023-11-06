19f1a19e3b310d82f39c084c3f4903c00b3a6edb
PatrickSpecial <47539464+PatrickSpecial@users.noreply.github.com>
2023-11-06 04:58:18 +0100

    added responsiveness

--- src/Shared/Strings.tsx
+++ src/Shared/Strings.tsx
@@ -1,5 +1,5 @@
 export default{
-    ABOUT_HEADING:'Hi! My name is Patrick Ubong Kingsley,\n I’m React Frontend Developer based in Nigeria',
+    ABOUT_HEADING:'Hi! My Name is Patrick Ubong Kingsley,\n I’m React Frontend Developer based in Nigeria',
     ABOUT_DESC:"As a dedicated React developer, I'm deeply enthusiastic about creating innovative and user-friendly web applications. React has captured my heart with its ability to build dynamic and responsive user interfaces, and I'm continuously motivated to explore its ever-evolving ecosystem. My passion for React extends beyond coding; it's about crafting delightful user experiences and solving complex problems. I thrive on staying up-to-date with the latest industry trends and best practices, always seeking to improve and adapt my skills. Collaborating with teams and contributing to open-source projects are some of the ways I channel my passion for React to make a positive impact on the web development community.",
 
     SERVICES:'Services',
